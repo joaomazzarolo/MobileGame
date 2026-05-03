f3be1e7017e4475e8bdf83bfea025601af96e692
joaomazzarolo <41448205+joaomazzarolo@users.noreply.github.com>
2026-05-03 17:15:51 +0200

    created levels and level pieces

---
+++ Assets/Scripts/LevelManager/LevelManager.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public Transform container;
+    public List<GameObject> levels;
+    public List<LevelPieceBase> levelPiecesStart;
+    public List<LevelPieceBase> levelPieces;
+    public List<LevelPieceBase> levelPiecesEnd;
+    public int piecesNumberStart = 3;
+    public int piecesNumber = 5;
+    public int piecesNumberEnd = 1;
+
+    [SerializeField] private int _index;
+
+    private List<LevelPieceBase> _spawnedPieces;
+    private GameObject _currentLevel;
+
+    private void Awake()
+    {
+        //SpawnNextLevel();
+        CreateLevelPieces();
+    }
+
+    private void SpawnNextLevel()
+    {
+        if (_currentLevel != null)
+        {
+            Destroy(_currentLevel);
+            _index++;
+            if (_index >= levels.Count)
+            {
+                ResetLevelIndex();
+            }
+        }
+        _currentLevel = Instantiate(levels[_index], container);
+        _currentLevel.transform.position = Vector3.zero;
+    }
+
+    private void ResetLevelIndex()
+    {
+        _index = 0;
+    }
+
+    private void CreateLevelPieces()
+    {
+        _spawnedPieces = new List<LevelPieceBase>();
+
+        for (int i = 0; i < piecesNumberStart; i++)
+        {
+            CreateLevelPiece(levelPiecesStart);
+        }
+        for (int i = 0; i < piecesNumber; i++)
+        {
+            CreateLevelPiece(levelPieces);
+        }
+        for (int i = 0; i < piecesNumberEnd; i++)
+        {
+            CreateLevelPiece(levelPiecesEnd);
+        }
+    }
+
+    private void CreateLevelPiece(List<LevelPieceBase> list)
+    {
+        var piece = list[Random.Range(0, list.Count)];
+        var spawnedPiece = Instantiate(piece, container);
+
+        if (_spawnedPieces.Count > 0)
+        {
+            var lastPiece = _spawnedPieces[_spawnedPieces.Count - 1];
+
+            spawnedPiece.transform.position = lastPiece.endPiece.position;
+        }
+        _spawnedPieces.Add(spawnedPiece);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            SpawnNextLevel();
+        }
+    }
+}
